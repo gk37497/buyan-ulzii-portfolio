19bc39069a44b2d3c7dca247c176fc071eddfcc5
Namsrai <namsraijamts@mid.mn>
2023-03-27 17:08:47 +0800

    some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,14 @@ export default function Index() {
       <div className="flex flex-col space-y-5 md:flex-row">
         <div className="w-full space-y-5">
           <Text variant="large">
-               I'm a freshmen of University of Finance and Economics in Mongolia and majoring in economics. I love being part of a team and working on a project. 
+            I am a freshmen of University of Finance and Economics in Mongolia
+            and majoring in economics. I love being part of a team and working
+            on a project.
           </Text>
           <Text variant="large">
-               For my spare time, I enjoy listening a music and hanging out with my friends. I have a wide academic knowledge and power skills that distinguishes me from my peers.
+            For my spare time, I enjoy listening a music and hanging out with my
+            friends. I have a wide academic knowledge and power skills that
+            distinguishes me from my peers.
           </Text>
           <Anchor text="More about me ..." url="/about" />
         </div>
